45a1d6f3c0e9d60da197445fef16a3ce3ce5e94a
Kevin Hsu <kevin.hsu@ubnt.com>
2018-11-05 20:12:08 +0800

    Update title and upload wizard strings (#10)
    * Update title
    
    * Update description font and color
    
    * Adjust strings in upload wizard

--- src/App.vue
+++ src/App.vue
@@ -31,8 +31,10 @@ export default {
 </script>
 
 <style lang="scss">
+  @import url(//fonts.googleapis.com/earlyaccess/notosanstc.css);
+
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: 'Noto Sans TC', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
